5f01419a893f473a72210f7e01df36e8f97dcdde
AdrienBariau <adrienbariau@gmail.com>
2017-12-14 18:41:11 +0100

    Trap

---
+++ AnkamaJam/Assets/Scripts/Game/DamageTrap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine; 
+public class DamageTrap : TrapModel
+{
+	[SerializeField]
+	private int m_damage; 
+
+	public override void Activate(CharacterBehaviour character) {
+		character.Damage (m_damage);
+	}	
+}
+
---
+++ AnkamaJam/Assets/Scripts/Game/TrapModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System;
+
+public abstract class TrapModel : MonoBehaviour
+{
+	[SerializeField]
+	private int m_cooldown;
+
+	public abstract void Activate(CharacterBehaviour c);
+}
+
