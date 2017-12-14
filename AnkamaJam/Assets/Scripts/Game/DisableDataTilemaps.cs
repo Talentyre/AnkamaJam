f52caa2d3873140f55f38188046ef15165ddd6ae
Clement Corvisy <ccorvisy@ankama.lan>
2017-12-14 22:41:37 +0100

    Disable Data TileMaps

---
+++ AnkamaJam/Assets/Scripts/Game/DisableDataTilemaps.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableDataTilemaps : MonoBehaviour
+{
+	public List<GameObject> GameObjectToDisable;
+
+	private void Start()
+	{
+		GameObjectToDisable.ForEach(g => g.SetActive(false));
+	}
+}
