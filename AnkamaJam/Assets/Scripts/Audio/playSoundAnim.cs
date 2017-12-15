c4fe1214823db00462ceac907566a5efcdb152c6
Axxaris <34451191+Axxaris@users.noreply.github.com>
2017-12-15 11:37:35 +0100

    Script Sound Animation
    Script associé aux Anim

---
+++ AnkamaJam/Assets/Scripts/Audio/playSoundAnim.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundAnim : StateMachineBehaviour {
+
+    public AudioClip ClipToPlay;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var audioSource = animator.gameObject.GetComponent<AudioSource>();
+        audioSource.clip = ClipToPlay;
+        audioSource.Play();
+    }
+}
