08b79cad39c2bfdc52f3a353eeefa2d99a02f378
Clement Corvisy <ccorvisy@ankama.lan>
2017-12-15 21:07:22 +0100

    Alert Gauge + Armoire

--- AnkamaJam/Assets/Scripts/UI/GameSceneUI.cs
+++ AnkamaJam/Assets/Scripts/UI/GameSceneUI.cs
@@ -11,7 +11,11 @@ public class GameSceneUI : MonoBehaviour
 	public Text ComboText;
 	public Text SoulText;
 	public Image SoulGauge;
-	public Image AlertGauge;
+	public Image AlertGauge1;
+	public Image AlertGauge2;
+	public Image AlertGauge3;
+	public Image AlertGauge4;
+	public Image AlertGauge5;
 	private Tweener _alertTweener;
 	private Vector2 _soulGaugeBaseSizeDelta;
 
@@ -24,10 +28,17 @@ public class GameSceneUI : MonoBehaviour
 		var doPunchScale = ComboText.transform.DOPunchScale(Vector3.one * 0.1f, 0.25f).Pause();
 		GameSingleton.Instance.OnAlertUpdate += f =>
 		{
-			AlertGauge.fillAmount = (float)f/GameSingleton.MaxAlert;
-			if (AlertGauge.fillAmount > 0.9 && _alertTweener == null)
+			var alertPercent = (float)f/GameSingleton.MaxAlert;
+			var alerteGaugeToBlink = GetAlerteGaugeToBlink(alertPercent);
+			if (alerteGaugeToBlink != null  && _alertTweener == null)
 			{
-				_alertTweener = AlertGauge.DOFade(0f, 1f).OnComplete(() => AlertGauge.DOFade(1f, 1f)).SetLoops(-1);
+				_alertTweener = alerteGaugeToBlink.DOFade(0f, 1f).OnComplete(() => alerteGaugeToBlink.DOFade(1f, 1f)).SetLoops(-1);
+			}
+			var alerteGaugeToActive = GetAlerteGaugeToActive(alertPercent);
+			if (alerteGaugeToActive != null)
+			{
+				alerteGaugeToActive.DOKill();
+				alerteGaugeToActive.color = Color.white;
 			}
 		};
 		GameSingleton.Instance.OnSoulUpdate += f =>
@@ -53,6 +64,56 @@ public class GameSceneUI : MonoBehaviour
 		{
 			ScoreText.text = f.ToString();
 		};
+	}
+
+	public Image GetAlerteGaugeToBlink(float percent)
+	{
+		if (percent > 0.15 && percent < 0.2)
+		{
+			return AlertGauge1;
+		}
+		if (percent > 0.35 && percent < 0.4)
+		{
+			return AlertGauge2;
+		}
+		if (percent > 0.55 && percent < 0.6)
+		{
+			return AlertGauge3;
+		}
+		if (percent > 0.75 && percent < 0.8)
+		{
+			return AlertGauge4;
+		}
+		if (percent > 0.95 && percent < 1)
+		{
+			return AlertGauge5;
+		}
+		return null;
+	}
+
+	public Image GetAlerteGaugeToActive(float percent)
+	{
+		if (percent < 0.4)
+		{
+			return AlertGauge1;
+		}
+		if (percent < 0.6)
+		{
+			return AlertGauge2;
+		}
+		if (percent < 0.8)
+		{
+			return AlertGauge3;
+		}
+		if (percent < 1)
+		{
+			return AlertGauge4;
+		}
+		if (percent >= 1)
+		{
+			return AlertGauge5;
+		}
+		return null;
 	}
 
 	public void QuitGame()
